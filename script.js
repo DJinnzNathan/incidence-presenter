a39e047908848bd6641becd3e21ef4058e7f4885
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2021-05-24 17:50:47 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const API = "https://api.corona-zahlen.org/districts/09563/history/incidence/1";
+
+getIncidence();
+
+async function getIncidence() {
+    fetch(API)
+    .then(response => response.json())
+    .then(response => {
+        let incidence = response.data['09563'].history[0]['weekIncidence'];
+        let incidenceNr = incidence;
+        incidence = Number(incidence).toFixed(1).replace('.', ',');
+        document.getElementById('incidence').innerText = incidence;
+
+        let incidenceDate = Date.parse(response.data['09563'].history[0]['date']);
+        incidenceDate = new Date(incidenceDate).toLocaleDateString('de');
+        document.getElementById('incidence_date').innerText = "Stand: " + incidenceDate;
+        
+        if(incidenceNr < 50) {
+            document.getElementById('incidence_icon').innerText = '⚽';
+        }
+    });
+}
