3c23cfbca0ec224eb63252242d21bf817d1460d7
D-Jinnz Nathan <djinnz.nathan@gmail.com>
2021-05-25 20:29:11 +0200

    icon changes, if incidence is under 100 for 5 days

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API = "https://api.corona-zahlen.org/districts/09563/history/incidence/2";
+const API = "https://api.corona-zahlen.org/districts/09563/history/incidence/5";
 
 getIncidence(API);
 
@@ -7,9 +7,11 @@ async function getIncidence(inputData) {
     .then(response => response.json())
     .then(response => {
         let incidence, incidenceDate = 0;
+        let playable = true;
         for(let i = 0; i < response.data['09563'].history.length; i++) {
             incidence = response.data['09563'].history[i]['weekIncidence'];
             incidenceDate = Date.parse(response.data['09563'].history[i]['date']);
+            if(incidence >= 100) playable = false;
         }
         let incidenceNr = incidence;
         incidence = Number(incidence).toFixed(1).replace('.', ',');
@@ -20,7 +22,7 @@ async function getIncidence(inputData) {
         
         const hands = ['👆🏻', '👆🏼', '👆🏽', '👆🏾', '👆🏿'];
 
-        if(incidenceNr < 50) {
+        if(playable === true) {
             document.getElementById('incidence_icon').classList.remove('wave');
             document.getElementById('incidence_icon').classList.add('bounce');
             document.getElementById('incidence_icon').innerText = '⚽';
